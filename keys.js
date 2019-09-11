92e70c5b78e463b653d5095916b0115a6c31ff63
Calah Hansen <calah.hansen@gmail.com>
2019-09-11 12:48:47 -0500

    keys and env

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+exports.omdb = {
+  id: process.env.OMDB_ID,
+  secret: process.env.OMBD_SECRET
+};
+exports.bandsInTown = {
+  id: process.env.BIT_ID,
+  secret: process.env.BIT_SECRET
+};
