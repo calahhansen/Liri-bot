5bd429fe0a5db28424ecda0d6d787a0d392169bd
Calah Hansen <calah.hansen@gmail.com>
2019-09-11 21:39:58 -0500

    added spotify npm documentation example to get started

--- liri.js
+++ liri.js
@@ -11,35 +11,61 @@ require("dotenv").config();
 const keys = require("./keys.js");
 
 //Include the spotify npm package (run "npm install --save node-spotify-api")
-const Spotify = require("node-spotify-api");
+const Spotify = require("node-spotify-api"); //why does npm documentation capitalize Spotify?? This doesn't look like a constructor...
 const spotify = new Spotify(keys.spotify); 
 
+//How does this mix in with the env and keys.js?? copied from npm docs
+var spotify = new Spotify({
+  id:81fc3562f62e451bb39d0e0ff23296f1,
+  secret:7279e7eefbba4ebf9bad9898e04b9fc0,
+});
 
-
-//User input 1 from terminal (0 and 1 are rubbish and garbage)
-const input1 = process.argv[2];
-//User input 2 from the terminal
-const input2 = process.argv[3];
+//User input specifying movie, concert, song from terminal (0 and 1 are rubbish and garbage)
+const cmdType = process.argv[2];
+//User input specifying the artist/band, movie, or song search from the terminal
+const userSearch = process.argv[3];
 
 //node liri.js concert-this <artist/band name here> will search Bands in Town API for the following: name of venue, venue location, date of event (i.e. moment.js "MM/DD/YYYY")
+//npm docs say that you can use either method with spotify (search or request)....what is the difference?
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
 
 
 (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) 
 
 //node liri.js spotify-this-song '<song name here> will show the following: artist, song name, preview link, album name, if no song.....default "The Sign" by Ace of Base
-
-(https://www.npmjs.com/package/node-spotify-api)
+const getEvents = function (input1) {
+  if (input2 === "" || typeof input2 === typeof undefined) {
+    input2 = "Estelle";
+  }
+}
+// (https://www.npmjs.com/package/node-spotify-api)
 
 
 //node liri.js movie-this '<movie name here>' will show the following: movie name, year, imdb rating, rotten rating, country produced, language, plot and actors, if no movie....'Mr. Nobody.' movie info block
+// Grab the movieName which will always be the third node argument.
+var movieName = process.argv[2];
 
-//If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-//It's on Netflix!
+// Then run a request with axios to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-// CLASS EXAMPLE ----Then run a request with axios to the OMDB API with the movie specified (OK to use trilogy api key)
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
   function(response) {
-    console.log("The movie's rating is: " + response.data.imdbRating);
+    console.log("Release Year: " + response.data.Year);
+  })
+  .catch(function(error) {
+
+axios.get("http://www.omdbapi.com/?t=" + input3 "&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    console.log(response);
   })
   .catch(function(error) {
     if (error.response) {
@@ -61,6 +87,10 @@ axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=tr
     }
     console.log(error.config);
   });
+
+
+//If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
+//It's on Netflix!
 
   //node liri.js do-what-it-says`
 
